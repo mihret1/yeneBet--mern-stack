ceba9aec9ad815f6ba73fe05bdc76f03cc59c9b8
mihret1 <90724655+mihret1@users.noreply.github.com>
2024-02-28 00:31:01 +0300

    navbar for desktop size

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,24 +6,28 @@ function Navbar() {
   return (
 
 
-    <div className=" flex flex-row  h-[110px] items-center shadow-xl justify-between px-10 ">
+    <div className="hidden lg:flex flex-row  h-[110px] items-center shadow-xl justify-between px-10 ">
 
+      <a href='/'>
       <div className='flex items-center gap-1'>
         <img src={img1} className='w-20 h-20 rounded-full'/>
         <h1 className='font-bold text-3xl font-mono	'>YeneBet</h1>
       </div>
+      </a>
 
       <div className='flex flex-row gap-5'>
-        <a href='/' className='text-lg'> Type/Sefer</a>
-        <a href='/' className='text-lg'> AboutUs</a>
-        <a href='/' className='text-lg'> Service</a>
-        <a href='/' className='text-lg'>Blog</a>
-        <a href='/' className='text-lg'> Contact</a>
+        <a href='/' className='text-xl'> Type/Sefer</a>
+        <a href='/' className='text-xl'> AboutUs</a>
+        <a href='/' className='text-xl'> Service</a>
+        <a href='/' className='text-xl'>Blog</a>
+        <a href='/' className='text-xl'> Contact</a>
       </div>
 
-      <button className='bg-[#ff6900] px-9 py-1 text-white font-medium	text-xl'>Login</button>
+      <button className='bg-[#ff6900] px-10 py-1 text-white font-medium	text-xl'>Login</button>
       
     </div>
+  
+
   
   
     )
